d5542d3c17f888d073cbcbe82a4a4cbc52a80ccb
Hung Huy <huyhung23072006@gmail.com>
2026-08-23 11:32:06 +0700

    BT Tuan 2

---
+++ Tuan2/NHH/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NHH.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
